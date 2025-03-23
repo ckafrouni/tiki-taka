04310cad2827947daa1a26a218aa8e9459bd97ad
Christophe Kafrouni <44137603+ckafrouni@users.noreply.github.com>
2025-03-23 15:00:35 +0100

    finish

--- src/app/beta/page.tsx
+++ src/app/beta/page.tsx
@@ -5,47 +5,91 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen">
-      <header className="bg-gray-800 text-white p-4">
-        <h1 className="text-xl font-bold">Choose your task</h1>
-      </header>
+    <div className="h-screen w-screen overflow-hidden bg-neutral-900 text-white">
+      <div className="container mx-auto flex flex-col gap-8 justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold text-center text-blue-200">
+          Tiki-Taka
+        </h1>
 
-      <main className="flex-1 overflow-y-auto p-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {Object.keys(cognitiveDiversity).map((taskName) => (
-            <Link key={taskName} href={`/beta/${taskName}`} className="block">
-              <button className="w-full p-4 text-left bg-white shadow-lg rounded-lg hover:shadow-xl transition-shadow">
-                {/* replace camelcase with readable shit */}
-                <h2 className="text-lg font-semibold capitalize">
-                  {taskName.replace(/([A-Z])/g, " $1").trim()}
-                </h2>
-              </button>
-            </Link>
-          ))}
+        <div className="flex flex-col gap-16">
+          <div className="flex flex-row gap-8">
+            <p className="flex-1 text-justify">
+              {
+                "We don't believe the popular notion idea-generation is a magical human capability and our collective MOAT over AI."
+              }
+              <br />
+              {
+                "When you hear the word efficient brainstorming, you think about about unique insights coming from unique perspectives. This is the benefit of cognitive diversity - different thinking patterns, different focus points - the same comes for political or philosophical debates in the form of views."
+              }
+              <br />
+              {
+                "A single AI will never create deeply contrarian and unique takes - its 'diffused' personality will only generate shallow ideas that are hard to disprove to avoid opposing itself but are also hardly valuable."
+              }
+              <br />
+              {
+                "That is why such discussions are better with a cluster of AIs. They challenge takes, build upon existing ones, and ask you the right questions - instead of parroting the most obvious ideas, it makes a deep exploration into the idea maze."
+              }
+              <br />
+              {
+                "AIs don't have the many biases we have and their lack of egos makes sure progress is done, no matter how many swear words are thrown around."
+              }
+              <br />
+            </p>
+
+            <p className="flex-1 text-justify">
+              {
+                "There are many applications which all require unique combination of thinking patterns. By capturing the views of the human, we can position the AIs perfectly asymmetrical to them."
+              }
+              <br />
+              {
+                "Conversations should be lively - dynamic turn-taking and interruption mechanisms are the future. Not just from the performance standpoint - this is the first time we can make 'discussion games'. Imagine trying to win an argument against Nietzsche and Aristotle about what the coolest Pokemon is."
+              }
+              <br />
+              {
+                "Currently we employ a flexible approach, where the user can decide whether and how he participates in the conversation. More specific flows like forcing him to answer specific questions or have him simply observe the automatic loops are interesting things to explore."
+              }
+              <br />
+              {
+                "Interestingly, arguments and ideas naturally let a tree structure emerge - graphs views would be cool."
+              }
+              <br />
+            </p>
+          </div>
+
+          <div className="flex flex-row gap-6 justify-center">
+            {Object.keys(cognitiveDiversity).map((taskName) => (
+              <Link
+                key={taskName}
+                href={`/beta/${taskName}`}
+                className="flex items-center justify-center text-neutral-900 bg-blue-200 hover:bg-blue-300 w-full p-4 text-center text-2xl font-semibold capitalize rounded-xl transition-colors"
+              >
+                {taskName.replace(/([A-Z])/g, " $1").trim()}
+              </Link>
+            ))}
+          </div>
         </div>
 
-        <p>
- I don't believe the popular notion idea-generation is a magical human capability and our collective MOAT over AI. I personally benefit from 
-
-When you hear the word efficient brainstorming, you think about about unique insights coming from unique perspectives. This is the benefit of cognitive diversity - different thinking patterns, different focus points - the same comes for political or philosophical debates in the form of views.
-
-A single AI will never create deeply contrarian and unique takes - its "diffused" personality will only generate shallow ideas that are hard to disprove to avoid opposing itself but are also hardly valuable.
-
-That is why such discussion are better with a cluster of AIs. They challenge takes, build upon existing ones, and ask you the right questions - instead of parroting the most obvious ideas, it makes a deep exploration into the idea maze.
-
-AIs don't have the many biases we have and their lack of egos makes sure progress is done, no matter how many swear words are thrown around.
-
---- 
-
-There are many application which all require unique combination of thinking patterns. By capturing the views of the human, we can position the AIs perfectly assymetrical to them.
-
-Conversations should be lively - dynamic turn-taking and interruption mechanisms are the future...
-
-Currently we employ a flexible approach, where the user can decide whether and how he participates in the conversation. More specific flows like forcing him to answer specific questions or have him simply observe the automatic loops are interesting things to explore.
-
-Interestingly, arguments and ideas naturally let a tree structure emerge - graphs views would be cool.
-        </p>
-      </main>
+        <div className="flex flex-col gap-2">
+          <p className="text-center text-neutral-400">
+            This was build during the EBL hackathon in Munich {"<3"}
+          </p>
+          <p className="text-center text-neutral-400">
+            <Link
+              href="https://github.com/ckafrouni"
+              className="text-blue-200 hover:text-blue-300"
+            >
+              Chris
+            </Link>
+            <br />
+            <Link
+              href="https://github.com/codeScourge"
+              className="text-blue-200 hover:text-blue-300"
+            >
+              Ivan
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
